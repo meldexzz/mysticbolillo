75b5f00b5fdf5b8aeaa76b5743743023e10f51ae
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-10 18:40:47 -0600

    Update fix-esperando_mensaje.js

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -1,43 +1,32 @@
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
-
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync, readFileSync } from 'fs';
+import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.fix_esperando_mensage
-
-  if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: tradutor.texto1}, {quoted: m});
-  }
-  const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
-  const sessionPath = './MysticSession/';
-  try {
-    const files = await fs.readdir(sessionPath);
-    let filesDeleted = 0;
-    for (const file of files) {
-      for (const id of chatId) {
-        if (file.includes(id.split('@')[0])) {
-          await fs.unlink(path.join(sessionPath, file));
-          filesDeleted++;
-          break;
-        }
-      }
-    }
-    if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: tradutor.texto2}, {quoted: m});
-    } else {
-      await conn.sendMessage(m.chat, {text: `${tradutor.texto3[0]} ${filesDeleted} ${tradutor.texto3[1]}`}, {quoted: m});
-    }
-  } catch (err) {
-    console.error(tradutor.texto4, err);
-    await conn.sendMessage(m.chat, {text: tradutor.texto5}, {quoted: m});
-  }
-  await conn.sendMessage(m.chat, {text: `${tradutor.texto6} \n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+if (global.conn.user.jid !== conn.user.jid) {
+return conn.sendMessage(m.chat, {text: '*⚠️ Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m})}
+const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
+const sessionPath = './sessions'
+try {
+const files = await fs.readdir(sessionPath);
+let filesDeleted = 0;
+for (const file of files) {
+for (const id of chatId) {
+if (file.includes(id.split('@')[0])) {
+await fs.unlink(path.join(sessionPath, file));
+filesDeleted++;
+break;
+}}} 
+if (filesDeleted === 0) {
+await conn.sendMessage(m.chat, {text: '*No se encontró ningún archivo que incluya la ID del chat*'}, {quoted: m});
+} else {
+await conn.sendMessage(m.chat, {text: `*Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m})
+}} catch (err) {
+console.error('Error al leer la carpeta o los archivos de sesión:', err);
+await conn.sendMessage(m.chat, {text: '𝘼𝙧𝙘𝙝𝙞𝙫𝙤𝙨 𝙙𝙚 𝙨𝙚𝙨𝙞ó𝙣 𝙚𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤𝙨 𝙘𝙤𝙧𝙧𝙚𝙘𝙩𝙖𝙢𝙚𝙣𝙩𝙚. 🥖'}, {quoted: m});
+}
+await conn.sendMessage(m.chat, {text: `¿𝙔𝙖 𝙢𝙚 𝙫𝙚𝙨, 𝙗𝙤𝙡𝙞𝙡𝙡𝙤?. 🥖`}, {quoted: m});
 };
-handler.help = ['fixmsgespera'];
-handler.tags = ['fix'];
+handler.help = ['ds']
+handler.tags = ['tools']
 handler.command = /^(fixmsgespera|ds)$/i;
 export default handler;
