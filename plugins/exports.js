e85dd3719d08d9822cd5c5fe4ea8afb807f94e02
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-11 12:05:06 -0600

    Delete plugins/exports.js

--- plugins/exports.js
+++
@@ -1,8 +0,0 @@
-
-export const apis = {
-  delirius: 'https://delirius-apiofc.vercel.app/',
-  siputzx: 'https://api.siputzx.my.id/api/',
-  ryzen: 'https://apidl.asepharyana.cloud/',
-  rioo: 'https://restapi.apibotwa.biz.id/',
-  random1: 'https://api.agungny.my.id/api/'
-};
