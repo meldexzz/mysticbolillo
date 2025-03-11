d7a13fb5ba17e0a786ada9a952a80a838d0bae6f
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-10 19:33:48 -0600

    Add files via upload

---
+++ plugins/exports.js
@@ -0,0 +1,8 @@
+
+export const apis = {
+  delirius: 'https://delirius-apiofc.vercel.app/',
+  siputzx: 'https://api.siputzx.my.id/api/',
+  ryzen: 'https://apidl.asepharyana.cloud/',
+  rioo: 'https://restapi.apibotwa.biz.id/',
+  random1: 'https://api.agungny.my.id/api/'
+};
